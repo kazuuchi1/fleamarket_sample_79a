1952f84864385846792b80f7eb930d9d176a99e1
kazuuchi1 <68491740+kazuuchi1@users.noreply.github.com>
2020-10-28 03:14:49 +0900

    addheader

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  get 'top/index'
+  #root 'items#index'
   # root 'buyers#new'
+   root 'top#index'
   resources :buyers, only: [:new, :create]
 end
