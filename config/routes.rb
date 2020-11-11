db4942a40f9ad6f5ad263708ea596a4396b524d1
BeE15 <sotaimo15@gmail.com>
2020-11-11 20:45:56 +0900

    replace4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-  root 'items#index'
   resources :mypages, only: [:show] do
     collection do
       get 'logout'
