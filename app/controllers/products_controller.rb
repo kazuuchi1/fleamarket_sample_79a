fed899e1bb7217a266fed043924bae0b93adcee7
harumisa <h.a.r.u.ponpon814@gmail.com>
2020-11-12 16:37:02 +0900

    使用しなくなった不要な記述を削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,6 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @categories = @product.categories
-    @user = User.find(params[:id])
     @product_image = ProductImage.where(params[:ids]).first
     @product_images = ProductImage.all.where(params[:ids])
   end
