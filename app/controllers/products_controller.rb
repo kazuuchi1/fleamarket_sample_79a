79ff1b3cbad00e956c24f394dc61324cb29d1e7c
harumisa <h.a.r.u.ponpon814@gmail.com>
2020-11-13 19:40:43 +0900

    product_image.idをproduct.idでfindしていたので、product_image.product_idからfindして、findしたimageを表示するように修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,13 @@ class ProductsController < ApplicationController
   
   def index
     @products = Product.all
-    @product_image = ProductImage.find_by(params[:id])
+    @product_images = ProductImage.all
   end
 
   def show
     @product = Product.find(params[:id])
     @categories = @product.categories
-    @product_image = ProductImage.find_by(params[:id])
+    @product_image = ProductImage.find_by(product_id: params[:id])
     @product_images = ProductImage.all.where(params[:ids])
   end
 
