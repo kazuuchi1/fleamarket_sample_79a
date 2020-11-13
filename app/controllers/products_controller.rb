f7e92a91337e9d5064d9a7a7fc4f4a97f04d3d0f
harumisa <h.a.r.u.ponpon814@gmail.com>
2020-11-13 15:59:53 +0900

    where(params[:ids]).firstの記述を、find_byを使った記述に変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,13 @@ class ProductsController < ApplicationController
   
   def index
     @products = Product.all
-    @product_image = ProductImage.where(params[:ids]).first
+    @product_image = ProductImage.find_by(params[:id])
   end
 
   def show
     @product = Product.find(params[:id])
     @categories = @product.categories
-    @product_image = ProductImage.where(params[:ids]).first
+    @product_image = ProductImage.find_by(params[:id])
     @product_images = ProductImage.all.where(params[:ids])
   end
 
